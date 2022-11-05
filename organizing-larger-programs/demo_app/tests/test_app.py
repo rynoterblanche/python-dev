02ec6c0cbbb4959c628beaa5e387d9c5ba8bfb1b
rynoterblanche <ryno.terblanche@gmail.com>
2022-11-05 12:41:27 +0000

    update test

--- organizing-larger-programs/demo_app/tests/test_app.py
+++ organizing-larger-programs/demo_app/tests/test_app.py
@@ -1,12 +1,22 @@
 import unittest
 
+from demo_app.infrastructure.on_prem_worker import OnPremWorker
+
+from demo_app.infrastructure.cloud_worker import CloudWorker
+
 from demo_app.app import App
 
 
 class TestApp(unittest.TestCase):
 
     def test_start_with_cloud_config(self):
-        App('config.cloud').start()
+        app = App('config.cloud')
+        app.start()
+
+        self.assertTrue(isinstance(app.worker, CloudWorker))
 
     def test_start_with_onprem_config(self):
-        App('config.onprem').start()
+        app = App('config.onprem')
+        app.start()
+
+        self.assertTrue(isinstance(app.worker, OnPremWorker))
